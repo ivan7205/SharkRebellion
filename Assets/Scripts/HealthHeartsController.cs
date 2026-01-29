36939d8da81356986292cc0afd50cbd8a3155ca2
Yolanda Fdez. <yolandafp53@educastur.es>
2026-01-29 13:38:23 +0100

    Jeff Prefab

---
+++ Assets/Scripts/HealthHeartsController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthHeartsController : MonoBehaviour
+{
+    public int Health;
+    public int maxHealth;
+
+    public Sprite emptyHeart;
+    public Sprite fullHeart;
+    public Image[] hearts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

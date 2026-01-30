5e971b8b58e74916d1150c1fd8e416e213c1c2c0
ivan7205 <ivanga51@educastur.es>
2026-01-30 13:53:46 +0100

    fondoooooo

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+pvoid Awake()
+{
+}
+posx target_poseX + derechaMax;
+posY = target_posey + alturaMin;
+transform.position Vector3.Lerp(transform.position, new Vector3(posX, posY, -1), 1);
+1 referencia
+void Move_Cam()
+{
+if (encendida)
+{
+if (target)
+{
+target_posex target.transform.position.x;
+target_poseY = target.transform.position.y;
+if (target_poseX > derechaMax && target_poseX < izquierdaMax)
+{
+    posX target_posex;
+    if (target_poseY < alturaMax && target_poseY > alturaMin)
+    {
+        posY target_poseY;
+        transform.position = Vector3.Lerp(transform.position, new Vector3(posX, posy, -1), speed * Time.deltaTime);
+        // Update is called once per frame
+        Mensaje de Unity | O referencias
+    void Update()
+        {
+            Move Cam():
